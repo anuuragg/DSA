c5526e4df82bbf419e0c1c788cc7b58c8adc4c79
Anurag <anuragcpyadav031@gmail.com>
2026-01-10 22:54:31 +0530

    more patterns

--- patterns.c
+++ patterns.c
@@ -129,7 +129,6 @@ void mixedStarHalf(int n){
         }
         printf("\n");
     }
-    printf("\n");
 }
 
 void binaryTriangle(int n){
@@ -141,33 +140,229 @@ void binaryTriangle(int n){
         }
         printf("\n");
     }
-    printf("\n");
+}
+
+void symetricNumTriangle(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d", j);
+        }
+        for(int j = 0; j <= 2*n - (2*i + 1); j++){
+            printf(" ");
+        }
+        for(int j = i; j >= 1; j--){
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+}
+
+void linearTriangle(int n){
+    int count = 1;
+    for(int i = 0; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("%d ", count);
+            count++;
+        }
+        printf("\n");
+    }
+}
+
+void alphabetTriangle(){
+    char alphabets[5] = {'A', 'B', 'C', 'D', 'E'};
+    for(int i = 0; i <= 4; i++){
+        for(int j = 0; j <= i; j++){
+            printf("%c", alphabets[j]);
+        }
+        printf("\n");
+    }
+}
+
+void revAlphabetTriangle(){
+    char alphabets[5] = {'A', 'B', 'C', 'D', 'E'};
+    for(int i = 5; i >= 0; i--){
+        for(int j = 0; j < i; j++){
+            printf("%c", alphabets[j]);
+        }
+        printf("\n");
+    }
+}
+
+void alphabetTriangleM(){
+    char alphabets[5] = {'A', 'B', 'C', 'D', 'E'};
+    for(int i = 0; i <= 4; i++){
+        for(int j = 0; j <= i; j++){
+            printf("%c", alphabets[i]);
+        }
+        printf("\n");
+    }
+}
+
+void alphabetTriangleP(){
+    char alphabets[5] = {'A', 'B', 'C', 'D', 'E'};
+    for(int i = 0; i <= 3; i++){
+        for(int j = 0; j <= 3-i-1; j++){
+            printf(" ");
+        }
+
+        for(int j = 0; j <= i; j++){
+            printf("%c", alphabets[j]);
+        }
+        
+        for(int j = i-1; j >= 0; j--){
+            printf("%c", alphabets[j]);
+        }
+        
+        for(int j = 0; j <= 3-i-1; j++){
+            printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+void halfAlphabetTriangle(){
+    char alphabets[5] = {'A', 'B', 'C', 'D', 'E'};
+    for(int i = 0; i <= 4; i++){
+        int f = 4;
+        for(int j = 0; j <= i; j++){
+            printf("%c ", alphabets[f]);
+            f--;
+        }
+        printf("\n");
+    }
+}
+
+void innerSpcePattern(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= n-i-1; j++){
+            printf("*");
+        }
+        for(int j = 2; j <= 2*i+1; j++){
+            printf(" ");
+        }
+        for(int j = 0; j <= n-i-1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        for(int j = 0; j < 2*n - (2*i); j++){
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void outterSpcePattern(int n){
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        for(int j = 0; j < 2*n - (2*i); j++){
+            printf(" ");
+        }
+        for(int j = 1; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j <= n-i-1; j++){
+            printf("*");
+        }
+        for(int j = 2; j <= 2*i+1; j++){
+            printf(" ");
+        }
+        for(int j = 0; j <= n-i-1; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+void starBorder(int n){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(j == 0 || j == n-1 || i == 0 || i == n-1){
+                printf("*");
+            } else printf(" ");
+        }
+        printf("\n");
+    }
 }
 
 int main(){
     
     squareStarPattern(5);
     printf("\n");
+
     triangleStarPattern(5);
     printf("\n");
+
     triangleNumberPattern(5);
     printf("\n");
+
     tNumberPattern(5);
     printf("\n");
+
     revStarPattern(5);
     printf("\n");
+
     revNumberPattern(5);
     printf("\n");
+
     starTriangle(5);
     printf("\n");
+
     revStarTriangle(5);
     printf("\n");
+
     mixedStarTriangle(5);
     printf("\n");
+
     mixedStarHalf(5);
     printf("\n");
+    
     binaryTriangle(5);
     printf("\n");
 
+    symetricNumTriangle(4);
+    printf("\n");
+
+    linearTriangle(5);
+    printf("\n");
+
+    alphabetTriangle();
+    printf("\n");
+
+    revAlphabetTriangle();
+    printf("\n");
+
+    alphabetTriangleM();
+    printf("\n");
+
+    alphabetTriangleP();
+    printf("\n");
+    
+    halfAlphabetTriangle();
+    printf("\n");
+
+    innerSpcePattern(5);
+    printf("\n");
+    
+    outterSpcePattern(5);
+    printf("\n");
+
+    starBorder(4);
+    printf("\n");
+    
     return 0;
 }
